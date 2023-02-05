5f389ef507c13ffad01ea1d70f8b76f72bf3378b
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-05 10:19:54 -0800

    create experience page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
+import Experiences from './pages/Experiences/Experiences'
 
 // components
 import NavBar from './components/NavBar/NavBar'
@@ -51,6 +52,14 @@ const App = () => {
           element={
             <ProtectedRoute user={user}>
               <Profiles />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/experiences"
+          element={
+            <ProtectedRoute user={user}>
+              <Experiences />
             </ProtectedRoute>
           }
         />
---
+++ src/pages/Experiences/Experiences.jsx
@@ -0,0 +1,7 @@
+const Experiences = (props) => {
+  return (  
+    <h1>This is experience Page</h1>
+   );
+}
+ 
+export default Experiences;
