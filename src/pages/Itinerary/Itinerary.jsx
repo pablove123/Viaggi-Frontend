2461096c34b22c1c61c9c885e763ab6f8897283d
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-06 15:38:50 -0800

    Add itetinerray states and addFunctions

--- src/pages/Itinerary/Itinerary.jsx
+++ src/pages/Itinerary/Itinerary.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
+import ExperienceList from "../ExperienceList/ExperienceList"
+
 function Itinerary(props) {
+  
+const [romeExp, setRomeExp] = useState([])
+const [veniceExp, setVeniceExp] = useState([])
+const [florenceExp, setFlorenceExp] = useState([])
+
+const addToRome = (experience) => {
+  setRomeExp([...romeExp, experience])
+}
+const addToVenice = (experience) => {
+  setVeniceExp([...veniceExp, experience])
+}
+const addToFlorence = (experience) => {
+  setFlorenceExp([...florenceExp, experience])
+}
+
   return(
-    <h1>Itinerary</h1>
+    <>
+    <ExperienceList/>
+    <h1>Rome</h1>
+    <h1>Venice</h1>
+    <h1>Florence</h1>
+    </>
   )
 }
 
