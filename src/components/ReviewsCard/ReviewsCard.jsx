5b1456b42d200fa08628e1398794b58b73bf2750
kevinharr <kevinharr@verizon.net>
2023-02-07 09:41:43 -0500

    redo review card

--- src/components/ReviewsCard/ReviewsCard.jsx
+++ src/components/ReviewsCard/ReviewsCard.jsx
@@ -1,7 +1,20 @@
-function ReviewsCard(props) {
-    return (
-      <h1>Review Card Page</h1>
-    )
-  }
-  
-  export default ReviewsCard
+import AuthorInfo from "../AuthorInfo/AuthorInfo"
+
+const ReviewCard = ({ review }) => {
+  console.log("this is the prop called review kph:", review.content)
+  return (
+    <article>
+      <header>
+      </header>
+      <p>{review.content}</p>
+      <p>{review.rating}</p>
+      <p>{review.owner}</p>
+      <h1>Hello</h1>
+      {/* <AuthorInfo author={review} /> */}
+    </article>
+
+        
+  )
+}
+
+export default ReviewCard
