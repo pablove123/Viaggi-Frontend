587790a097b3e04ab8fc6fb3af68f4bb50663d3f
kevinharr <kevinharr@verizon.net>
2023-02-08 07:58:24 -0500

    pass handleDeleteExperience to ExperienceDetails

--- src/App.jsx
+++ src/App.jsx
@@ -163,6 +163,15 @@ const App = () => {
 		    <EditExperience handleUpdateExperience={handleUpdateExperience} />
 	      </ProtectedRoute>
         } />
+
+        <Route path="/experiences/:id" element={
+        <ProtectedRoute user={user}>
+        <ExperienceDetails user={user} handleDeleteExperience={handleDeleteExperience} />
+        </ProtectedRoute>
+        } />
+
+
+
       </Routes>
     </>
   )
