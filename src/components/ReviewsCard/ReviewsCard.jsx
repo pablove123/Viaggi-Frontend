48b63529eb02cdd16bb84bb62a552ddc5e887460
alex-jones <abjhoops@gmail.com>
2023-02-09 01:19:42 -0700

    add buffer under reviews

--- src/components/ReviewsCard/ReviewsCard.jsx
+++ src/components/ReviewsCard/ReviewsCard.jsx
@@ -2,16 +2,19 @@ import styles from './ReviewsCard.module.css'
 
 const ReviewCard = ({ review }) => {
   return (
-    <main className={styles.container}>
-      <div>
-        <img src={review.author.photo} alt="" />
-      </div>
-      <div>
-        <h3>{review.author.name}</h3>
-        <p>{review.rating} ⭐️ Rating</p>
-        <p>{review.content}</p>
-      </div>
-    </main>
+    <>
+      <main className={styles.container}>
+        <div>
+          <img src={review.author.photo} alt="" />
+        </div>
+        <div>
+          <h3>{review.author.name}</h3>
+          <p>{review.rating} ⭐️ Rating</p>
+          <p>{review.content}</p>
+        </div>
+      </main>
+      <div className={styles.buffer}></div>
+    </>
 
         
   )
