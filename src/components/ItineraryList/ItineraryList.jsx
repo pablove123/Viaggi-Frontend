d6c7a7f6546860052a11e4f7af7915da666baa5c
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-08 19:44:59 -0800

    add link to itnerary details

--- src/components/ItineraryList/ItineraryList.jsx
+++ src/components/ItineraryList/ItineraryList.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+
 function ItineraryList(props) {
+  const { id } = useParams()
+  console.log("this is props", props)
+
+  // useEffect(() => {
+  //   const fetchExperience = async () => {
+  //     const data = await expService.show(id)
+  //     setExperience(data)
+  //   }
+  //   fetchExperience()
+  // }, [id])
+  
     return (
       <section>
         <h1>Itinerary List Page</h1>
         {props.myIts.map((itinerary)=>(
-          <p key={itinerary._id}>{itinerary.name}</p>
+          //  <p key={itinerary._id}>{itinerary.name}</p>
+          <Link to={`${itinerary._id}`}><p key={itinerary._id}>{itinerary.name}</p></Link>
         ))}
       </section>
     )
